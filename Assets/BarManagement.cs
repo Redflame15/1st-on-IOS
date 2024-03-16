cb025140b9cabf3fc3102e6095d3b74f766aa63d
Redflame15 <letai18072000@gmail.com>
2024-03-15 22:33:09 -0700

    Project transfer

---
+++ Assets/BarManagement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManagement : MonoBehaviour
+{
+    public GameObject bar;
+    Collider2D col;
+    Collider2D touchCollider;
+    Touch touch;
+    Vector2 touchPosition;
+
+    //public void ToggleVisibility()
+    //{
+    //    hBar.SetActive(false);
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+        bar.Visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchCollider = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchCollider)
+                {
+                    bar.Visible = true;
+                }
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Visibility.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visibility : MonoBehaviour
+{
+    public GameObject bar;
+
+    Vector2 touchPosition;
+    Touch touch;
+    Collider2D collider;
+    Collider2D touchCollider;
+
+    //public void ToggleVisibility()
+    //{
+
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        bar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (2 > Input.touchCount && Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchCollider = Physics2D.OverlapPoint(touchPosition);
+                if (collider == touchCollider)
+                {
+                    bar.SetActive(true);
+                }
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                
+            }
+
+        }
+    }
+}
